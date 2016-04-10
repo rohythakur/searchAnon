00a914d4d9d9c7547ca1cef0f2209062c9d98843
eddwinn <eddwinn@gmail.com>
2016-04-10 17:01:06 -0400

    Updated to run Continously

--- spider/browseReddit.py
+++ spider/browseReddit.py
@@ -1,49 +1,44 @@
-import time
-import os
 import json
-from datetime import datetime
-import sys, os
-import argparse
-from os.path import basename
+import os
+import time
 
 
-folderPath = '/home/logic/PycharmProjects/onionChaser/logs/Onions/'
+folderPath = '/home/logic/PycharmProjects/onionChaser/onionChaser/spiders/logs/Onions/'
 searchFor = ['.ONION', '.ONION/']
+print searchFor
+print folderPath
+
 def run_bot():
 
-        json_files = [pos_json for pos_json in os.listdir(folderPath) if pos_json.endswith('.json')]
-        #print json_files  # for me this prints ['foo.json']
+    json_files = [pos_json for pos_json in os.listdir(folderPath)]
 
-        for js in json_files:
-            if not js.endswith('.old'):
+
+    for js in json_files:
+        if js.endswith('.old'):
+            print "No data"
+            continue
+
+        else:
+            try:
                 with open(os.path.join(folderPath, js)) as json_file:
+
                     json_text = json.load(json_file)
                     for idof in json_text:
                         if idof['url'].endswith(tuple(searchFor)):
                             x = idof['url']
-                            yield x
-                    #print json.dumps(json_text, indent=4)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    #path = os.path.join(folderPath, js)
-                    #target = os.path.join(folderPath, js + '.old')
-                    #os.rename(path, target)
-                    #print x
+                            print x
+                            #yield x
+                            try:
+                                path = os.path.join(folderPath, js)
+                                target = os.path.join(folderPath, js + '.old')
+                                os.rename(path, target)
+                            except OSError:
+                                pass
+            except ValueError:
+                time.sleep(10)
+                run_bot()
+while True:
+    run_bot()
+    time.sleep(3000)
 
 
