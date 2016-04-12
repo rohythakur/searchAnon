d16f5f3d473936d552e4af3934d62cc9808d11e9
eddwinn <eddwinn@gmail.com>
2016-04-11 21:48:50 -0400

    Database Links

--- spider/browseReddit.py
+++ spider/browseReddit.py
@@ -1,44 +1,64 @@
 import json
 import os
 import time
-
+from app.models import Item
+from app import db
 
 folderPath = '/home/logic/PycharmProjects/onionChaser/onionChaser/spiders/logs/Onions/'
 searchFor = ['.ONION', '.ONION/']
-print searchFor
-print folderPath
+print "RUN"
 
-def run_bot():
+def add_bot():
 
     json_files = [pos_json for pos_json in os.listdir(folderPath)]
-
-
     for js in json_files:
         if js.endswith('.old'):
             print "No data"
             continue
-
         else:
-            try:
-                with open(os.path.join(folderPath, js)) as json_file:
 
-                    json_text = json.load(json_file)
-                    for idof in json_text:
-                        if idof['url'].endswith(tuple(searchFor)):
-                            x = idof['url']
-                            print x
-                            #yield x
-                            try:
-                                path = os.path.join(folderPath, js)
-                                target = os.path.join(folderPath, js + '.old')
-                                os.rename(path, target)
-                            except OSError:
-                                pass
-            except ValueError:
-                time.sleep(10)
-                run_bot()
+            with open(os.path.join(folderPath, js)) as json_file:
+
+                json_text = json.load(json_file)
+                for idof in json_text:
+                    if idof['url'].endswith(tuple(searchFor)):
+                        x = idof['url']
+                        #print x
+                        #checkDouble = Item.query.filter(Item.link).first()
+                        #print checkDouble
+                        ##TODO FIX QUERY SEE IF EXISTS
+                        if Item.query(Item.id).filter(Item.link==Item.link).count() > 0:
+
+                            if x.endswith('/'):
+
+                                print "adding " + x
+                                links = Item(link=x)
+                                db.session.add(links)
+                                db.session.commit()
+
+                            else:
+                                newx= x + "/"
+
+                                print "adding " + newx
+                                links = Item(link=newx)
+                                db.session.add(links)
+                                db.session.commit()
+                        else:
+                            print x +"  already entered"
+
+
+                        #path = os.path.join(folderPath, js)
+                        #target = os.path.join(folderPath, js + '.old')
+                        #os.rename(path, target)
+
+
+
+
+
 while True:
-    run_bot()
-    time.sleep(3000)
+    add_bot()
+    time.sleep(4)
+
+
 
 
