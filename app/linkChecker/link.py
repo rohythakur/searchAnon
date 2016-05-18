793bc96e3e84d4f50a8232f28434657094f6db4f
eddwinn <edwin.eames214@gmail.com>
2016-05-17 21:39:05 -0400

    Updating rows in database from links

--- app/linkChecker/link.py
+++ app/linkChecker/link.py
@@ -6,6 +6,8 @@ from search.definebrowser import *
 from search.general import *
 from search.urlPrint import *
 from search.grabHtml import *
+from app.models import Item
+from app import db
 
 link = Item.query.all()
 
@@ -21,12 +23,25 @@ for u in Item.query.with_entities(Item.link):
     urls_map = dict(urls)
 
     def startcrawl():
+        ##TODO Update database where link = title, etc
         browser = get_browser(binary=firefox_binary, proxy=proxy)
         for resource in keys:
             browser.get(urls_map.get(resource))
-            printUrls(browser)
+            #printUrls(browser)
+            print (str(u))
+            html = browser.page_source
 
-            printhtml(browser)
+            soup = BeautifulSoup(html, "html5lib")
+            currentlink = str(u)
+            print (currentlink + " this is the currentlink")
+            for tag in soup.find_all('title'):
+                x = (tag.text)
+
+                currentrequest = Item.query.filter(Item.link==currentlink)
+                print (str(currentrequest))
+                currentrequest.title = u'hello'
+
+                db.session.commit()
 
 
     if __name__ == '__main__':
--- app/linkChecker/search/grabHtml.py
+++ app/linkChecker/search/grabHtml.py
@@ -3,20 +3,29 @@ from __future__ import print_function
 from datetime import datetime
 import os
 from bs4 import BeautifulSoup
+from sqlalchemy.orm import sessionmaker
+
+from app.models import Item
+from app import db
+
+
 fname =  datetime.now().strftime("%Y%m%d-%H%M%S") + ".txt"
 subdir = '/home/logic/Documents/data'
 
 
-def printhtml(browser):
+def printhtml(browser, u):
     html = browser.page_source
 
     soup = BeautifulSoup(html, "html5lib")
-
+    currentlink = str(u)
+    print (currentlink + " this is the currentlink")
     for tag in soup.find_all('title'):
-        print(tag.text)
-
-
-
+        x = (tag.text)
+        #print (x)
+        #session = sessionmaker()
+        currentrequest = db.session.query(Item).get(currentlink).update({"title": "hello"})
+        currentrequest.title = 'hello'
+        db.session.commit()
 
 
 
